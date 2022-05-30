96b1d46ba0b8679209b88bc8c0ab260fef66f8f9
Suijun <853573918g@gmail.com>
2022-05-29 22:36:47 -0400

    Change read format

--- explore.py
+++ explore.py
@@ -2,13 +2,17 @@
 
 import pandas as pd
 
+# Load file
 f = open('en-fr.csv', 'rb')
 
+# EDA
 for i in range(5):
     line = f.readline()
     print(line)
 
-df = pd.read_csv('en-fr.csv',nrows=1000, encoding='utf-8')
-df.head(10)
+df = pd.read_csv('en-fr.csv',nrows=2000, encoding='utf-8')
+print(df.head(10))
+output = df.values.tolist()
+print(output)
 
 pass
